1378d0b76a1c631ce4cdfd7cad287c05546a3fa3
zd-zero <zaerald.zd@gmail.com>
2018-06-14 23:50:35 +0800

    Add Progressbar using tqdm

--- src/__init__.py
+++ src/__init__.py
@@ -1,18 +1,23 @@
 #! python3
 
 import os
+import math
 import requests
+from tqdm import tqdm
 from bs4 import BeautifulSoup
 
 
 def download_from_url(url):
-    image_request = requests.get(url)
-    image_name = os.path.basename(image_url)
+    image_name = os.path.basename(url)
     print('Downloading ' + image_name + '...')
-
+    image_request = requests.get(url, stream=True)
     image_file = open(os.path.join('devhumor', image_name), 'wb')
-    for chunk in image_request.iter_content(100000):
-        image_file.write(chunk)
+    total_size = int(image_request.headers.get('content-length', 0))
+    block_size = 1024
+    for data in tqdm(image_request.iter_content(block_size),
+                     total=math.ceil(total_size//block_size),
+                     unit='KB', unit_scale=True):
+        image_file.write(data)
 
 
 print('Downloading Latest Devhumor Pictures...')
